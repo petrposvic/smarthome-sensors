6a35318d343526301cceb279ec2ef89ce7a5c966
Petr Posvic <petr@posvic.cz>
2016-12-21 10:21:44 +0100

    Use Dallas library

--- temp_out/temp_out.ino
+++ temp_out/temp_out.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h> // https://github.com/esp8266/Arduino
 #include <OneWire.h>
+#include <DallasTemperature.h>
 ADC_MODE(ADC_VCC);
 
 #define PIN 2
@@ -12,6 +13,7 @@ const int   port = 3000;
 
 // 4.7K resistor is necessary
 OneWire ds(PIN);
+DallasTemperature sensors(&ds);
 
 void setup(void) {
   Serial.begin(115200);
@@ -34,6 +36,8 @@ void setup(void) {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  sensors.begin();
 }
 
 void loop(void) {
@@ -94,87 +98,7 @@ void send_data(float t, float h, float v) {
 }
 
 float get_temp() {
-  byte i;
-  byte present = 0;
-  byte type_s;
-  byte data[12];
-  byte addr[8];
-
-  int counter = 0;
-  while (!ds.search(addr)) {
-    ds.reset_search();
-    delay(250);
-
-    if (++counter > 8) {
-      Serial.println("No more addresses!");
-      return -1000;
-    }
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-    Serial.println("CRC is not valid!");
-    return -2000;
-  }
-
-  switch (addr[0]) {
-    // DS18S20 or old DS1820
-    case 0x10:
-      type_s = 1;
-      break;
-
-    // DS18B20
-    case 0x28:
-      type_s = 0;
-      break;
-
-    // DS1822
-    case 0x22:
-      type_s = 0;
-      break;
-
-    // Not a DS18x20 family device
-    default:
-      return -3000;
-    }
-
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);  // Start conversion, with parasite power on at the end
-
-  delay(1000);        // Maybe 750ms is enough, maybe not
-  // We might do a ds.depower() here, but the reset will take care of it.
-
-  present = ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);     // Read Scratchpad
-
-  for (i = 0; i < 9; i++) {
-    data[i] = ds.read();
-  }
-
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
-    }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
-
-  // Celsius
-  return (float) raw / 16.0;
-
-  // Fahrenheit
-  // return ((float) raw / 16.0) * 1.8 + 32.0;
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
 }
+
